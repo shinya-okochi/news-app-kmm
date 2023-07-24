9654956de669c857c7468d1f1da2af923ed737b8
shinya-okochi <shny.kch@gmail.com>
2023-07-24 19:21:06 +0900

    Create project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "news-app-kmm"
+include(":androidApp")
+include(":shared")
