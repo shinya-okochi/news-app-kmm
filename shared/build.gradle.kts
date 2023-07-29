11e671394df4bb9e56ab091c1aca8ce3eca1c490
shinya-okochi <shny.kch@gmail.com>
2023-07-29 23:48:27 +0900

    API通信に必要なライブラリ・フレームワークの導入

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    // Serialization
+    kotlin("plugin.serialization") version embeddedKotlinVersion
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
@@ -25,10 +27,29 @@ kotlin {
         }
     }
 
+    val ktorVersion = "2.3.2"
+
     sourceSets {
         val commonMain by getting {
             dependencies {
-                //put your multiplatform dependencies here
+                // Coroutines
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+                // Ktor
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                // Ktor
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
+            }
+        }
+        val iosMain by getting {
+            dependencies {
+                // Ktor
+                implementation("io.ktor:ktor-client-darwin:$ktorVersion")
             }
         }
         val commonTest by getting {
